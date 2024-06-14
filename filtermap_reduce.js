e8c66243f48e77d18761c3550aec4a0e4b416d3a
Muthukumar V <www.goe2004@gmail.com>
2024-06-14 20:17:15 +0530

    change in examples

--- filtermap_reduce.js
+++ filtermap_reduce.js
@@ -1,54 +1,27 @@
-const num1 = [2, 3, 4, 5, 6, 7];
-// // const newNum=num1.filter( (num) =>num>4 ) // here return is not required
-// // console.log(newNum);
 
-// const newNum2=num1.filter((num)=> {
-//    return  num>4    //in parenthesis use return
-// })
-// console.log(newNum2);
+// In JavaScript, filter(), map(), and reduce()
+//are array methods that allow you to perform 
+//operations on arrays  in a functional programming style.
 
-// const newNum3=[]
-// num1.forEach((num)=>{
-//     if (num>4) {
-//         newNum3.push(num)
-//     }
-// })
-// console.log(newNum3);
+const numbers = [1, 2, 3, 4, 5];
+const numberFour = numbers.filter(num => num % 4 === 0); 
+console.log(numberFour)
 
-// const chocolates=[{
-//     chocolate:"Dairy milk", Price:20
-// },
-// {
-//     chocolate:"Munch",Price:10
-// },
-// {
-// chocolate:"5 star",Price:5
-// }]
+const fourNumbers = numbers.map(num => num * 4); 
+console.log(fourNumbers)
 
-// const choco =chocolates.filter((ch)=>ch.chocolate==="5 star")
-// console.log(choco);
+const sum = numbers.reduce((acc, num) => acc + num, 0); 
 
-//Map
-
-// const numbers=[1,2,3,4,5]
-// const numm=numbers.map((num)=>num+1)
-// //console.log(numm);
-
-// //chaining
-
-// const newnum=numbers.map((num)=>num+20).map((num)=>num*10).filter((num)=>num>150).filter((num)=>num>180) // at last if you put map for conditions you will get true or false put filter to get values
-// //console.log(newnum);
-
-// //reduce
-
-const reduceFunction = [1, 2, 3, 4, 5];
-// //Calculates the total value by reducing an array of numbers.
-// const mytotal=reduceFunction.reduce((acc,curr)=> acc+curr,0)
-// console.log(mytotal);
-
-// map->alters the original array if you use condition you will get only boolean values in array
-const total = reduceFunction.map((num) => num + 2);
-console.log(total);
-// filter -> checks condition if you try to alter you will only get the original array back without any change
-const total1 = reduceFunction.filter((num) => num > 2);
-console.log(total1);
+const products = [
+    { name: 'Product A', price: 10 },
+    { name: 'Product B', price: 20 },
+    { name: 'Product C', price: 15 },
+    { name: 'Product D', price: 25 },
+  ];
+  
+  const totalCost = products
+    .filter(product => product.price > 15) 
+    .map(product => product.price) 
+    .reduce((acc, price) => acc + price, 0); 
+  
+  console.log(totalCost); 
